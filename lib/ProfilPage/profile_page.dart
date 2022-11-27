288a2b465ab90f1313b99550bc7edcb6d06c1610
Raditya <ishariyanto598@gmail.com>
2022-11-27 09:19:12 +0700

    [RR] update ProfilePage

--- lib/ProfilPage/profile_page.dart
+++ lib/ProfilPage/profile_page.dart
@@ -7,98 +7,121 @@ class ProfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding:
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding:
               const EdgeInsets.only(left: 50, right: 50, bottom: 20, top: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/images/Warm Jacket.png'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Sauka Raditya",
-                style: GoogleFonts.lato(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 24,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "saukaraditya@gmail.com",
-                style: GoogleFonts.lato(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.yellow[400],
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(30),
-                    )),
-                height: 50,
-                width: 500,
-                child: Center(
-                  child: Text(
-                    "Upgrade To Premium",
-                    style: GoogleFonts.lato(
-                        fontSize: 20, fontWeight: FontWeight.w500),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.all(25),
+                      child: Text(
+                        "My Account",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                      child: CircleAvatar(
+                        radius: 100,
+                        backgroundImage: AssetImage('assets/images/Warm Jacket.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Sauka Raditya",
+                    style: GoogleFonts.lato(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "saukaraditya@gmail.com",
+                    style: GoogleFonts.lato(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.yellow[400],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        )),
+                    height: 50,
+                    width: 500,
+                    child: Center(
+                      child: Text(
+                        "Upgrade To Premium",
+                        style: GoogleFonts.lato(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ProfileMenu(
+                    text: 'Order History',
+                    icon: Icons.shopping_bag_outlined,
+                    arrowShown: true,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ProfileMenu(
+                    text: 'Help and Supprot',
+                    icon: Icons.help_outline,
+                    arrowShown: true,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ProfileMenu(
+                    text: 'Load gift voucher',
+                    icon: Icons.card_giftcard,
+                    arrowShown: true,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ProfileMenu(
+                    text: 'Log Out',
+                    icon: Icons.logout,
+                    arrowShown: false,
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              ProfileMenu(
-                text: 'Order History',
-                icon: Icons.shopping_bag_outlined,
-                arrowShown: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ProfileMenu(
-                text: 'Help and Supprot',
-                icon: Icons.help_outline,
-                arrowShown: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ProfileMenu(
-                text: 'Load gift voucher',
-                icon: Icons.card_giftcard,
-                arrowShown: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ProfileMenu(
-                text: 'Log Out',
-                icon: Icons.logout,
-                arrowShown: false,
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -117,10 +140,9 @@ class ProfileMenu extends StatelessWidget {
         decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.all(
-              Radius.circular(30),
+              Radius.circular(20),
             )),
-        height: 50,
-        width: 500,
+        padding: EdgeInsets.all(20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -130,7 +152,7 @@ class ProfileMenu extends StatelessWidget {
                   padding: const EdgeInsets.only(left: 12.0),
                   child: Icon(
                     icon,
-                    size: 30,
+                    size: 22,
 
                   ),
                 ),
@@ -145,13 +167,13 @@ class ProfileMenu extends StatelessWidget {
             ),
             arrowShown
                 ? Padding(
-                    padding: const EdgeInsets.only(right: 12.0),
-                    child: Icon(
-                      Icons.arrow_forward,
-                      size: 30,
-                      color: Colors.black,
-                    ),
-                  )
+              padding: const EdgeInsets.only(right: 12.0),
+              child: Icon(
+                Icons.arrow_forward,
+                size: 22,
+                color: Colors.black,
+              ),
+            )
                 : Container(),
           ],
         ));
